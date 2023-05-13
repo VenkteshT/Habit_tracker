98b2eae7d3007b07c3463ac6311a870318042d87
Venktesh <111628674+VenkteshT@users.noreply.github.com>
2023-05-13 19:29:57 +0530

    Update passport-local-strategy.js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -2,41 +2,47 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../model/user");
 
-// local strategy options
-const opts = {
-  usernameField: "email",
-};
+// 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+          req.flash(
+            "error",
+            !user ? "user dose not exist" : "Invalid Password or username"
+          );
+          done(null, false);
+          return;
+        } else {
+          return done(null, user);
+        }
+      } catch (err) {
+        console.log("found error in passport js file:", err.message);
+        done(err);
+      }
+    }
+  )
+);
 
-// callback function for local startegy
-const cb = async (email, password, done) => {
-  // find the user and establish identitiy
-  try {
-    const user = await User.findOne({ email });
-    if (!user || password != user.password)
-      throw new Error(!user ? "User not found" : "password mismatch");
-    return done(null, user);
-  } catch (err) {
-    console.log("found error in passport js file:", err);
-    return done(err);
-  }
-};
-
-// local strategy
-passport.use(new LocalStrategy(opts, cb));
-
-//set user cookie
+// serilize user
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-// extract from cookie
+// deserilize user
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
-    return done(null, user);
+    done(null, user);
   } catch (err) {
-    console.log("foudn error in passport js file:", err);
-    done(err);
+    console.log("error in deserilizer", err.message);
+    cone(err);
   }
 });
 
